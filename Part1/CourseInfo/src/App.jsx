706ae064ffe09d31d00194775215a00e3459b90d
PrimeFord <bashfordbash@gmail.com>
2023-03-27 13:14:54 +0100

    feat Part 1.1 & 1.2

--- Part1/CourseInfo/src/App.jsx
+++ Part1/CourseInfo/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Contents from "./Components/Contents";
 import Total from "./Components/Total";
+import Header from "./Components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{course}</h1>
+      <Header />
       <Contents />
       <Total />
     </div>
